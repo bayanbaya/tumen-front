e2c9d6f858acf26ab1f440a6c272347ea44d00aa
educatornum <educatornum@gmail.com>
2025-10-10 16:33:20 +0800

    deploy 2nd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // <-- this makes it static (needed for GitHub Pages)
+  images: { unoptimized: true }, // disable next/image optimization
+  basePath: '/tumen-front', // <-- your repository name here
+  assetPrefix: '/tumen-front/',
   images: {
     remotePatterns: [
       {
