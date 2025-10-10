f68a7a475fc42ec766b262d4792055f58e023c8e
educatornum <educatornum@gmail.com>
2025-10-10 17:00:02 +0800

    some changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+const repoName = 'tumen-front';
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
-  basePath: '/tumen-front',
-  assetPrefix: '/tumen-front/',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
     remotePatterns: [
